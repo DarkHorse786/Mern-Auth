9b748c1b60691977a936e8e710bea851cb397866
DarkHorse786 <samiullahwaheed786@gmail.com>
2025-05-12 12:05:23 +0500

    navbar created

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar.jsx'
+import Header from '../components/Header.jsx'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-center bg-cover'>
       <Navbar/>
+      <Header />
     </div>
   )
 }
